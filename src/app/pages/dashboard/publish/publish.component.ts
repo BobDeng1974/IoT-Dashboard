3c27ef551d2189f5f7a3d245cf824ef26adf8d9c
harithzamri <harithtoikee96@gmail.com>
2019-11-21 21:34:39 +0800

    Retain Flag

--- src/app/pages/dashboard/publish/publish.component.ts
+++ src/app/pages/dashboard/publish/publish.component.ts
@@ -10,15 +10,22 @@ export class PublishComponent implements OnInit {
 
     message : string =" ";
     topic : string =" ";
+    checked = false;
   
   constructor(private mqttService: MqttService) { 
       console.log(this.mqttService);
   }
+  
+  toggle(checked: boolean){
+      this.checked = checked;
+  }
 
   public publish(topic:string, message:string)
   {
-      this.mqttService.unsafePublish(topic, message, {qos: 1, retain: true});
+      this.mqttService.unsafePublish(topic, message, {qos: 1, retain: this.checked});
   }
+
+  
 
   ngOnInit() {
   }
