d84bc872b48a14e53e4b421b7a1b137a6c20103c
harithzamri <harithtoikee96@gmail.com>
2019-11-25 10:58:02 +0800

    Add Cloud Module

---
+++ src/app/pages/cloud/cloud.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'ngx-cloud',
+    templateUrl: './cloud.component.html',
+    styleUrls:['./cloud.component.scss']
+})
+export class CloudComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() { 
+
+    }
+
+}
