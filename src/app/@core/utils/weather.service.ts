6daa16bbffd76ac5909e7049adea8287dd68c62e
harithzamri <harithtoikee96@gmail.com>
2019-11-27 11:56:36 +0800

    Add WeatherService

---
+++ src/app/@core/utils/weather.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+@Injectable()
+export class WeatherService {
+
+
+    apiKey = 'c48785e650f66855ffe18fe69eadf2df';
+    
+    url;
+    constructor(private http:HttpClient){
+        this.url = 'http://api.openweathermap.org/data/2.5/forecast?q=';
+        
+    }
+
+    getWeather(city, location){
+        return this.http.get(this.url+city+','+location+'&APPID='+this.apiKey)
+    }
+
+   
+
+
+}
