a0ffc873829dcd55ad1cb2cd0e83c1e8c1ce23eb
harithzamri <harithtoikee96@gmail.com>
2019-11-27 08:42:33 +0800

    Add OEE Component

---
+++ src/app/pages/oee/oee.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'ngx-oee',
+    styleUrls:['./oee.component.scss'],
+    templateUrl: './oee.component.html'
+})
+
+export class OeeComponent{
+    
+}
---
+++ src/app/pages/oee/oee.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { OeeComponent } from './oee.component';
+
+@NgModule({
+    imports: [
+
+    ],
+    declarations:[
+        OeeComponent,
+    ],
+})
+export class OeeModule{}
